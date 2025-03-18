31c12b03621b637bfa8c8154f368d604ff69c7b8
mokshith2 <80023651+mokshith2@users.noreply.github.com>
2025-03-17 21:17:14 -0400

    Update methods.java

--- methods.java
+++ methods.java
@@ -1,31 +1,35 @@
 import java.io.*;
-
-class methods 
+class methods
 {
-    public static String getString() throws IOException 
+    methods()
     {
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        String s = br.readLine();
-        return s;
+        
     }
-
-    public static char getChar() throws IOException 
-    {
-        String s = getString();
-        return s.charAt(0);
-    }
-
-    public int getInt() throws IOException 
-    {
-        String s = getString();
-        return Integer.parseInt(s);
-    }
-
-    public int getDouble() throws IOException 
-    {
-        String s = getString();
-        Double aDub = Double.valueOf(s);
-        return aDub.doubleValue();
-    }
+	public static String getString(String fileName) throws IOException
+	{
+		InputStreamReader isr = new InputStreamReader(new FileInputStream(fileName));
+        //InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String s = br.readLine();
+		return s;
+	}
+	
+	public static char getChar() throws IOException
+	{
+		String s = getString();
+		return s.charAt(0);
+	}
+	
+	public int getInt() throws IOException
+	{
+		String s = getString();
+		return Integer.parseInt(s);
+	}
+	
+	public int getDouble() throws IOException
+	{
+		String s = getString();
+		Double aDub = Double.valueOf(s);
+		return aDub.doubleValue();
+	}
 }
